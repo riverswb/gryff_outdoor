8d1eb1f8cb9a7240e93d7d0d00e27656f14ab26d
Brett Rivers <riverswb@gmail.com>
2017-01-04 14:00:35 -0700

    Item validation testing complete.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a title" do
+        invalid_item = Item.new(title: nil, description: "tree description",
+                                price: 99.95, image: "tree-picture.com")
+
+          expect(invalid_item).to be_invalid
+      end
+
+      it "is invalid without a description" do
+        invalid_item = Item.new(title: "tree", description: nil,
+                                price: 99.95, image: "tree-picture.com")
+
+          expect(invalid_item).to be_invalid
+      end
+
+      it "is invalid without a price" do
+        invalid_item = Item.new(title: "tree", description: "tree description",
+                                price: nil, image: "tree-picture.com")
+
+          expect(invalid_item).to be_invalid
+      end
+
+      it "is invalid without an image" do
+        invalid_item = Item.new(title: "tree", description: "tree description",
+                                price: 99.95, image: nil)
+
+          expect(invalid_item).to be_invalid
+      end
+    end
+  end
 end
