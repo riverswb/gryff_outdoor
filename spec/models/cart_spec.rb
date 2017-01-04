db321e015451bfd3b7c5a9cfb363e2b61a83dbce
David Knott <david1k1nott@gmail.com>
2017-01-04 13:58:16 -0700

    create model test for add item

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  it "has contents" do
-    cart = Cart.new({"1" => 1})
 
+  attr_reader :cart
+
+  before(:all) do
+    @cart = Cart.new({"1" => 1})
+  end
+  
+  it "has contents" do
     expect(cart.contents).to eq({"1" => 1})
+  end
+
+  it "can add items to cart" do
+    cart.add_item(1)
+    cart.add_item(2)
+    
+    expect(cart.contents).to eq({"1" => 2, "2" => 1})
   end
 end
