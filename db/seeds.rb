d6600a8f665e2d70eb930066d6592af62d14f745
Brett Rivers <riverswb@gmail.com>
2017-01-05 14:40:47 -0700

    Configures seeds file to use faker directly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
-FactoryGirl.create_list(:item, 10)
+category_1 = Category.create(name: "hiking")
+category_2 = Category.create(name: "fishing")
+category_3 = Category.create(name: "climbing")
+
+
+30.times do
+  Item.create(
+    title:       Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence,
+    price:       Faker::Commerce.price,
+    image: "http://lorempixel.com/400/200/",
+    category:    ([category_1, category_2, category_3].sample)
+  )
+end
