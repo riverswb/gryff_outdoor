61a03e978489e5c021de0d0d2e6ef48ddbaee5d0
Brett Rivers <riverswb@gmail.com>
2017-01-05 09:27:03 -0700

    Test for adding items to cart from index written

--- spec/features/visitor_can_view_items_spec.rb
+++ spec/features/visitor_can_view_items_spec.rb
@@ -15,4 +15,20 @@ RSpec.feature "Visitor can view all items", type: :feature do
     expect(page).to have_content(first_item.price)
     expect(page).to have_content(last_item.price)
   end
+
+  scenario "visitor adds item to cart from item index" do
+    create_list(:item, 6)
+    first_item = Item.first
+
+    visit items_path
+
+    click_on "Add to cart"
+
+    expect(page).to have_content("#{first_item.title} has been added to the cart")
+
+    visit cart_path
+
+    expect(page).to have_content(first_item.title)
+    expect(page).to have_content(first_item.price)
+  end
 end
