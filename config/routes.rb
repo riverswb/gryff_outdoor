9308232f8501733a6f1e69d42be4527fe0560a7e
David Knott <david1k1nott@gmail.com>
2017-01-06 11:31:05 -0700

    finish cart routes refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   root 'landing#index'
+  delete '/carts', to: "carts#destroy", as: "carts"
   get '/cart', to: "carts#show", as: "cart"
+  patch '/cart' => 'carts#update'
   resources :items, only: [:index, :show]
-  resources :carts, only: [:create, :destroy]
+  resources :carts, only: [:create]
   resources :users, only: [:new, :create]
   get '/dashboard', to: "users#show"
   get '/login', to: "sessions#new"
   get '/logout', to: "sessions#destroy"
   get '*category' => 'categories#index'
-  get '/' => 'landing#index'
-  patch '/carts/:id' => 'carts#update'
 end
