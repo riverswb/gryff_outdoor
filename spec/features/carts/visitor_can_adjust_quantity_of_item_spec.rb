bc729fc64baad46468c4a29e040b3e0b383b91df
Brett Rivers <riverswb@gmail.com>
2017-01-05 21:06:43 -0700

    Fixes: makes hound happy by removing double quotes where not necessary

--- spec/features/carts/visitor_can_adjust_quantity_of_item_spec.rb
+++ spec/features/carts/visitor_can_adjust_quantity_of_item_spec.rb
@@ -6,18 +6,18 @@ RSpec.feature 'Visitor can adjust the quantity of an item in the cart', type: :f
     item = create(:item)
 
     visit item_path(item)
-    click_on "Add Item"
-    click_on "View Cart"
+    click_on 'Add Item'
+    click_on 'View Cart'
 
     expect(page).to have_content(item.title)
-    expect(page).to have_content("1")
+    expect(page).to have_content('1')
     expect(page).to have_content(item.price)
 
-    click_on "+"
+    click_on '+'
 
     expect(page).to have_content("Total Cost: $#{item.price * 2}")
 
-    click_on "-"
+    click_on '-'
 
     expect(page).to have_content("Total Cost: $#{item.price}")
   end
