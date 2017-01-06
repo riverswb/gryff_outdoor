c2d7799771604c925948cf4a69c6ebcf5497c48d
David Knott <david1k1nott@gmail.com>
2017-01-05 21:17:48 -0700

    add login status to applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_cart
+  before_action :set_cart, :login_status
 
   def set_cart
     @cart = Cart.new(session[:cart])
+  end
+
+  def login_status
+    if session[:user]
+      @login_status = "Login"
+      @login_path = login_path
+    else
+      @login_status = "Logout"
+      @login_path = logout_path
+    end
   end
 end
