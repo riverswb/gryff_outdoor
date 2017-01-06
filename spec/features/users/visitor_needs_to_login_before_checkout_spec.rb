dd6c4d7b2f89fa45a326a95755d02974cb70b45c
David Knott <david1k1nott@gmail.com>
2017-01-05 22:14:49 -0700

    create cart saves visitor items test

--- spec/features/users/visitor_needs_to_login_before_checkout_spec.rb
+++ spec/features/users/visitor_needs_to_login_before_checkout_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "When a vistor visits cart page" do
+
+  attr_reader :item
+
   before(:all) do
-    create_list(:item, 10)
+    @items = create_list(:item, 10)
+    visit item_path(items[0])
+    click_button "Add Item"
+    click_button "Add Item"
+    click_on "View Cart"
   end
 
   scenario "they need to login or create an account" do
-    item = Item.first
-    visit item_path(item)
-    click_button "Add Item"
-    click_button "Add Item"
     click_on "View Cart"
 
     expect(page).to_not have_button "Checkout"
@@ -19,4 +22,28 @@ RSpec.feature "When a vistor visits cart page" do
 
     expect(current_path).to eq login_path
   end
+
+  scenario "they can create an account and their data is saved" do
+    click_on "View Cart"
+    click_on "Login or Create Account to Checkout"
+    click_on "Create Account"
+
+    fill_in "user[first_name]", :with => "David"
+    fill_in "user[last_name]", :with => "Knott"
+    fill_in "user[email]", :with => "david@gmail.com"
+    fill_in "user[password]", :with => "1234"
+    fill_in "user[password_confirmation]", :with => "1234"
+
+    click_on "Create Account"
+    click_on "View Cart"
+
+    expect(page).to have_content items[0].title
+    expect(page).to have_content items[0].price
+  end
+
+  # scenario "they can logout" do
+
+  # end
+
+
 end
