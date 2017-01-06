a8000c228e5c943938b974aba6c23ded05c79bdc
Brett Rivers <riverswb@gmail.com>
2017-01-05 21:04:00 -0700

    Fixes a silly extra tab

--- spec/features/carts/visitor_can_adjust_quantity_of_item_spec.rb
+++ spec/features/carts/visitor_can_adjust_quantity_of_item_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.feature 'Visitor can adjust the quantity of an item in the cart', type: :feature do
 
-    scenario 'as a visitor with an item in cart' do
-      item = create(:item)
+  scenario 'as a visitor with an item in cart' do
+    item = create(:item)
 
-      visit item_path(item)
-      click_on "Add Item"
-      click_on "View Cart"
+    visit item_path(item)
+    click_on "Add Item"
+    click_on "View Cart"
 
-      expect(page).to have_content(item.title)
-      expect(page).to have_content("1")
-      expect(page).to have_content(item.price)
+    expect(page).to have_content(item.title)
+    expect(page).to have_content("1")
+    expect(page).to have_content(item.price)
 
-      click_on "+"
+    click_on "+"
 
-      expect(page).to have_content("Total Cost: $#{item.price * 2}")
+    expect(page).to have_content("Total Cost: $#{item.price * 2}")
 
-      click_on "-"
+    click_on "-"
 
-      expect(page).to have_content("Total Cost: $#{item.price}")
-    end
+    expect(page).to have_content("Total Cost: $#{item.price}")
+  end
 end
