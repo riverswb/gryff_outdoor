d0ed3e7dc7bfcc502a8bdc927e543d9693f7dc2c
David Knott <david1k1nott@gmail.com>
2017-01-05 19:24:40 -0700

    create user can go to login page feature test

---
+++ spec/features/users/visitor_can_create_an_account_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature "When a user visits root path" do
+  scenario "they can login" do
+    visit "/"
+
+    expect(page).to have_link "Login"
+    
+    click_on "Login"
+
+    expect(current_path).to eq "/login"
+
+    fill_in "user[:username]", :with => "David"
+    fill_in "user[:password]", :with => "123"
+
+    expect(page).to have_link "Create Account"
+  end
+end
