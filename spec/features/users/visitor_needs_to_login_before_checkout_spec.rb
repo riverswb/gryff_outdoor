c66bb1acc17df46b8398d86fc0873cf5b17f8a94
David Knott <david1k1nott@gmail.com>
2017-01-05 22:08:08 -0700

    pass visit needs to login first test

--- spec/features/users/visitor_needs_to_login_before_checkout_spec.rb
+++ spec/features/users/visitor_needs_to_login_before_checkout_spec.rb
@@ -4,15 +4,15 @@ RSpec.feature "When a vistor visits cart page" do
   before(:all) do
     create_list(:item, 10)
   end
-  
+
   scenario "they need to login or create an account" do
     item = Item.first
-    visit ite_path(item)
+    visit item_path(item)
     click_button "Add Item"
     click_button "Add Item"
-    visit cart_path
+    click_on "View Cart"
 
-    expect(page).to_not have_link "Checkout"
+    expect(page).to_not have_button "Checkout"
     expect(page).to have_link "Login or Create Account to Checkout"
     
     click_on "Login or Create Account to Checkout"
@@ -20,13 +20,3 @@ RSpec.feature "When a vistor visits cart page" do
     expect(current_path).to eq login_path
   end
 end
-
-
-  scenario "a message is displayed" do
-    item = Item.first
-
-    visit item_path(item)
-    click_button "Add Item"
-
-    expect(page).to have_content "You now have 1 #{item.title}."
-  end
