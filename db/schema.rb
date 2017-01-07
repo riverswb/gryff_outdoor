04bb676020df56ea0337c5a2894afb01155d95cd
David Knott <david1k1nott@gmail.com>
2017-01-06 22:21:21 -0700

    create orderitem join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106030908) do
+ActiveRecord::Schema.define(version: 20170107045205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,19 @@ ActiveRecord::Schema.define(version: 20170106030908) do
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.index ["item_id"], name: "index_order_items_on_item_id", using: :btree
+    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string  "status"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -42,4 +55,7 @@ ActiveRecord::Schema.define(version: 20170106030908) do
   end
 
   add_foreign_key "items", "categories"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
 end
