2579cf56a3392dc42fdf64389548adc898959eac
Brett Rivers <riverswb@gmail.com>
2017-01-07 12:24:02 -0700

    Refactors the carts_controller show method slightly.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,11 +5,26 @@ class CartsController < ApplicationController
     @items = @cart.contents.map do |item, quantity|
       [Item.find(item), quantity]
     end
-    @checkout_msg = "Login or Create Account to Checkout"
-    @checkout_msg = "Checkout" if session[:user]
-    @checkout_path = login_path
-    @checkout_path = orders_path if session[:user]
+    @checkout_msg = message?
+    @checkout_path = path?
     @total_cost = @cart.total_cost
+  end
+
+  def message?
+    if session[:user]
+      "Checkout"
+    else
+      "Login or Create Account to Checkout"
+    end
+  end
+
+  def path?
+    if session[:user]
+      # new_order_path
+      orders_path
+    else
+      login_path
+    end
   end
 
   def create
