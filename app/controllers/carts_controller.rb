1c69a2ce4b3d3cb0b4858e014c821df1f71567c1
Brett Rivers <riverswb@gmail.com>
2017-01-07 11:58:09 -0700

    Creates ivar checkout_path in carts_controller, a dirty way to get what I want right now, should be refactored

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,6 +7,8 @@ class CartsController < ApplicationController
     end
     @checkout_msg = "Login or Create Account to Checkout"
     @checkout_msg = "Checkout" if session[:user]
+    @checkout_path = login_path
+    @checkout_path = orders_path if session[:user]
     @total_cost = @cart.total_cost
   end
 
