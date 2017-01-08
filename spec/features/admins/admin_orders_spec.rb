dc13cd9a47f9e55e11cfb7a5dd0fd0f966e16f73
David Knott <david1k1nott@gmail.com>
2017-01-07 18:56:35 -0700

    create default user can't see all orders test

--- spec/features/admins/admin_orders_spec.rb
+++ spec/features/admins/admin_orders_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.feature "When an admin visits all orders page" do
   context "as admin" do
     it "allows admin to see all orders by all users" do
-      user = User.create(email: "quidditchstar6969@gryffoutdoor.com",
-                        password: "slytherinsuxx",
-                        role: 1)
+      user = create(:user, role: 1)
+
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:admin)
       visit admin_all_orders_path
       expect(page).to have_content "All Orders"
+    end
+
+    it "default user does not see admin all orders" do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController)to receive(:current_user).and_return(:admin)
+      visit admin_all_orders_path
+      expect(page).to_not have_content "All Orders"
+      expect(page).to have_content "The page you were looking for doesn't exist"
     end
   end
 end
