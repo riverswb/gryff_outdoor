be7702caf17ce1f8fb0a1ebec9e133ba6ce48545
David Knott <david1k1nott@gmail.com>
2017-01-07 19:47:59 -0700

    remove failing test that another member is working on

--- spec/features/admins/admin_dashboard_access_spec.rb
+++ spec/features/admins/admin_dashboard_access_spec.rb
@@ -6,7 +6,8 @@ RSpec.feature "When an admin visits dashboard page" do
       user = create(:user, role: 1)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit admin_all_orders_path
+      visit admin_dashboard_path
+
       expect(page).to have_content "Admin Dashboard"
     end
 
@@ -14,13 +15,15 @@ RSpec.feature "When an admin visits dashboard page" do
       user = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-      visit admin_all_orders_path
+      visit admin_dashboard_path
+
       expect(page).to_not have_content "Admin Dashboard"
       expect(page).to have_content "The page you were looking for doesn't exist"
     end
 
-    it "visitor does not see admin dashboard"
-      visit admin_dashboard
+    it "visitor does not see admin dashboard" do
+      visit admin_dashboard_path
+
       expect(page).to_not have_content "Admin Dashboard"
       expect(page).to have_content "The page you were looking for doesn't exist"
     end
