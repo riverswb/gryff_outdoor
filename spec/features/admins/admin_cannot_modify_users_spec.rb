0eb5c87310a7418c44c1d4b19afa58c67971304a
Brett Rivers <riverswb@gmail.com>
2017-01-07 23:19:12 -0700

    Test for issue 17 written

---
+++ spec/features/admins/admin_cannot_modify_users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpe.feature 'Admins cannot edit users data' do
+  scenario 'as a logged in admin' do
+    user = create(:user)
+    admin = create(:user, role: 1)
+
+    visit login_path
+
+    fill_in "email", with: admin.email
+    fill_in "password", with: admin.password
+    click_button "Login"
+
+    visit user_path(admin)
+    click_on "Edit"
+
+    fill_in "email", with: "test@test.com"
+    click_on "Update"
+
+    expect(page).to have_content("Email: test@test.com")
+
+    visit user_path(user)
+
+    expect(page).to_not have_link("Edit")
+  end
+end
