8537f2bdd82f4cae09243135e8309a8c047ee5f4
Brett Rivers <riverswb@gmail.com>
2017-01-07 22:55:55 -0700

    Fixes hound complaints

--- spec/features/admins/admin_viewing_items_spec.rb
+++ spec/features/admins/admin_viewing_items_spec.rb
@@ -8,28 +8,28 @@ RSpec.feature 'When an admin views items index' do
 
     user = create(:user, role: 1)
     visit login_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Login"
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Login'
 
     expect(current_path).to eq(admin_dashboard_path)
 
-    click_on "View Items"
-    save_and_open_page
-     expect(current_path).to eq(admin_items_path)
+    click_on 'View Items'
 
-     expect(page).to have_link(items_active.first.title)
-     expect(page).to have_link(items_active.last.title)
-     expect(page).to have_link(inactive_item.title)
+    expect(current_path).to eq(admin_items_path)
 
-     expect(page).to have_content(items_active.first.description)
-     expect(page).to have_content(items_active.last.description)
-     expect(page).to have_content(inactive_item.description)
+    expect(page).to have_link(items_active.first.title)
+    expect(page).to have_link(items_active.last.title)
+    expect(page).to have_link(inactive_item.title)
 
-     expect(page).to have_content(items_active.first.status)
-     expect(page).to have_content(items_active.last.status)
-     expect(page).to have_content(inactive_item.status)
+    expect(page).to have_content(items_active.first.description)
+    expect(page).to have_content(items_active.last.description)
+    expect(page).to have_content(inactive_item.description)
 
-     expect(page).to have_link("Actions")
+    expect(page).to have_content(items_active.first.status)
+    expect(page).to have_content(items_active.last.status)
+    expect(page).to have_content(inactive_item.status)
+
+    expect(page).to have_link('Actions')
   end
 end
