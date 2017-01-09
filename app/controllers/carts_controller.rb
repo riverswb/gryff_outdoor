15e65c74e0d3d580c0b408515396522701662809
David Knott <david1k1nott@gmail.com>
2017-01-09 11:34:33 -0700

    refactor carts_controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -12,10 +12,8 @@ class CartsController < ApplicationController
 
   def create
     item = Item.find(params[:item_id])
-
     @cart.add_item(item.id)
     session[:cart] = @cart.contents
-
     flash[:notice] = "You now have #{pluralize(@cart.count_of(item.id), item.title)}."
     redirect_back fallback_location: items_path
   end
