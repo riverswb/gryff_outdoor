25c680d2989ea47184dd12b81bff612a260607c6
Ethan Bennett <ebennett19@gmail.com>
2017-01-09 14:27:15 -0700

    Add form for new address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,12 +6,24 @@ class AddressesController < ApplicationController
   end
 
   def create
+    @address = @user.addresses.new(address_params)
+    if @address.save
+      flash[:success] = "You successfully added an address to your account."
+      redirect_to dashboard_path
+    else
+      flash[:alert] = "Error! Make sure you filled in all the fields correctly."
+      render :new
+    end
   end
 
   def destroy
   end
 
 private
+
+  def address_params
+    params.require(:address).permit(:street_address, :secondary_address, :city, :state_abbr, :zip_code)
+  end
 
   def set_user
     @user = User.find(params[:user_id])
