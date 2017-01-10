688b2907804eb902c26f33b81f8dc3fb10194506
Brett Rivers <riverswb@gmail.com>
2017-01-09 21:39:37 -0700

    Makes hound happier

--- spec/features/admins/admin_cannot_modify_users_spec.rb
+++ spec/features/admins/admin_cannot_modify_users_spec.rb
@@ -9,171 +9,173 @@ RSpec.feature 'Admins cannot edit users data' do
   scenario 'as a logged in admin can edit own profile' do
     visit login_path
 
-    fill_in "email", with: @admin.email
-    fill_in "password", with: @admin.password
-    click_button "Login"
+    fill_in 'email', with: @admin.email
+    fill_in 'password', with: @admin.password
+    click_button 'Login'
 
     visit user_path(@admin)
-    click_on "Edit"
+    click_on 'Edit'
 
-    fill_in "user[first_name]", with: "Testy"
-    fill_in "user[last_name]", with: "McTesterson"
-    fill_in "user[email]", with: "test@test.com"
-    fill_in "user[password]", with: @admin.password
-    fill_in "user[password_confirmation]", with: @admin.password
+    fill_in 'user[first_name]', with: 'Testy'
+    fill_in 'user[last_name]', with: 'McTesterson'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: @admin.password
+    fill_in 'user[password_confirmation]', with: @admin.password
 
-    click_on "Update"
+    click_on 'Update'
 
-    expect(page).to have_content("Name: Testy McTesterson")
-    expect(page).to have_content("Email: test@test.com")
+    expect(page).to have_content('Name: Testy McTesterson')
+    expect(page).to have_content('Email: test@test.com')
   end
 
   scenario "As a logged in admin, cannot edit a user's profile" do
     visit login_path
 
-    fill_in "email", with: @admin.email
-    fill_in "password", with: @admin.password
-    click_button "Login"
+    fill_in 'email', with: @admin.email
+    fill_in 'password', with: @admin.password
+    click_button 'Login'
 
     visit admin_user_path(@user)
 
     expect(page).to have_content(@user.first_name)
     expect(page).to have_content(@user.last_name)
     expect(page).to have_content(@user.email)
-    expect(page).to_not have_link("Edit")
+    expect(page).to_not have_link('Edit')
   end
 
-  describe "SAD Path for edidting admin profile" do
-    scenario "deletes first name" do
+  describe 'SAD Path for edidting admin profile' do
+    scenario 'deletes first name' do
       visit login_path
 
-      fill_in "email", with: @admin.email
-      fill_in "password", with: @admin.password
-      click_button "Login"
+      fill_in 'email', with: @admin.email
+      fill_in 'password', with: @admin.password
+      click_button 'Login'
 
       visit user_path(@admin)
-      click_on "Edit"
+      click_on 'Edit'
 
-      fill_in "user[first_name]", with: nil
-      fill_in "user[last_name]", with: "McTesterson"
-      fill_in "user[email]", with: "test@test.com"
-      fill_in "user[password]", with: @admin.password
-      fill_in "user[password_confirmation]", with: @admin.password
+      fill_in 'user[first_name]', with: nil
+      fill_in 'user[last_name]', with: 'McTesterson'
+      fill_in 'user[email]', with: 'test@test.com'
+      fill_in 'user[password]', with: @admin.password
+      fill_in 'user[password_confirmation]', with: @admin.password
 
-      click_on "Update"
+      click_on 'Update'
 
       expect(current_path).to eq(edit_user_path(@admin))
       expect(page).to have_content("First name can't be blank")
     end
 
-    scenario "deletes last name" do
+    scenario 'deletes last name' do
       visit login_path
 
-      fill_in "email", with: @admin.email
-      fill_in "password", with: @admin.password
-      click_button "Login"
+      fill_in 'email', with: @admin.email
+      fill_in 'password', with: @admin.password
+      click_button 'Login'
 
       visit user_path(@admin)
-      click_on "Edit"
+      click_on 'Edit'
 
-      fill_in "user[first_name]", with: "Testy"
-      fill_in "user[last_name]", with: nil
-      fill_in "user[email]", with: "test@test.com"
-      fill_in "user[password]", with: @admin.password
-      fill_in "user[password_confirmation]", with: @admin.password
+      fill_in 'user[first_name]', with: 'Testy'
+      fill_in 'user[last_name]', with: nil
+      fill_in 'user[email]', with: 'test@test.com'
+      fill_in 'user[password]', with: @admin.password
+      fill_in 'user[password_confirmation]', with: @admin.password
 
-      click_on "Update"
+      click_on 'Update'
 
       expect(current_path).to eq(edit_user_path(@admin))
       expect(page).to have_content("Last name can't be blank")
     end
 
-    scenario "deletes email" do
+    scenario 'deletes email' do
       visit login_path
 
-      fill_in "email", with: @admin.email
-      fill_in "password", with: @admin.password
-      click_button "Login"
+      fill_in 'email', with: @admin.email
+      fill_in 'password', with: @admin.password
+      click_button 'Login'
 
       visit user_path(@admin)
-      click_on "Edit"
+      click_on 'Edit'
 
-      fill_in "user[first_name]", with: "Testy"
-      fill_in "user[last_name]", with: "McTesterson"
-      fill_in "user[email]", with: nil
-      fill_in "user[password]", with: @admin.password
-      fill_in "user[password_confirmation]", with: @admin.password
+      fill_in 'user[first_name]', with: 'Testy'
+      fill_in 'user[last_name]', with: 'McTesterson'
+      fill_in 'user[email]', with: nil
+      fill_in 'user[password]', with: @admin.password
+      fill_in 'user[password_confirmation]', with: @admin.password
 
-      click_on "Update"
+      click_on 'Update'
 
       expect(current_path).to eq(edit_user_path(@admin))
       expect(page).to have_content("Email can't be blank")
     end
 
-    scenario "Password left blank" do
+    scenario 'Password left blank' do
       visit login_path
 
-      fill_in "email", with: @admin.email
-      fill_in "password", with: @admin.password
-      click_button "Login"
+      fill_in 'email', with: @admin.email
+      fill_in 'password', with: @admin.password
+      click_button 'Login'
 
       visit user_path(@admin)
-      click_on "Edit"
+      click_on 'Edit'
 
-      fill_in "user[first_name]", with: "Testy"
-      fill_in "user[last_name]", with: "McTesterson"
-      fill_in "user[email]", with: "test@test.com"
-      fill_in "user[password]", with: nil
-      fill_in "user[password_confirmation]", with: @admin.password
+      fill_in 'user[first_name]', with: 'Testy'
+      fill_in 'user[last_name]', with: 'McTesterson'
+      fill_in 'user[email]', with: 'test@test.com'
+      fill_in 'user[password]', with: nil
+      fill_in 'user[password_confirmation]', with: @admin.password
 
-      click_on "Update"
+      click_on 'Update'
 
       expect(current_path).to eq(edit_user_path(@admin))
-      expect(page).to have_content("Incorrect password")
+      expect(page).to have_content('Incorrect password')
     end
 
     scenario "Password confirmation left blank / doesn't match password" do
       visit login_path
 
-      fill_in "email", with: @admin.email
-      fill_in "password", with: @admin.password
-      click_button "Login"
+      fill_in 'email', with: @admin.email
+      fill_in 'password', with: @admin.password
+      click_button 'Login'
 
       visit user_path(@admin)
-      click_on "Edit"
+      click_on 'Edit'
 
-      fill_in "user[first_name]", with: "Testy"
-      fill_in "user[last_name]", with: "McTesterson"
-      fill_in "user[email]", with: "test@test.com"
-      fill_in "user[password]", with: @admin.password
-      fill_in "user[password_confirmation]", with: nil
+      fill_in 'user[first_name]', with: 'Testy'
+      fill_in 'user[last_name]', with: 'McTesterson'
+      fill_in 'user[email]', with: 'test@test.com'
+      fill_in 'user[password]', with: @admin.password
+      fill_in 'user[password_confirmation]', with: nil
 
-      click_on "Update"
+      click_on 'Update'
 
       expect(current_path).to eq(edit_user_path(@admin))
-      expect(page).to have_content("Password confirmation doesn't match Password")
+      expect(page).to have_content("Password confirmation doesn't match
+                                    Password")
     end
 
-    scenario "Password and confirmation do not match" do
+    scenario 'Password and confirmation do not match' do
       visit login_path
 
-      fill_in "email", with: @admin.email
-      fill_in "password", with: @admin.password
-      click_button "Login"
+      fill_in 'email', with: @admin.email
+      fill_in 'password', with: @admin.password
+      click_button 'Login'
 
       visit user_path(@admin)
-      click_on "Edit"
+      click_on 'Edit'
 
-      fill_in "user[first_name]", with: "Testy"
-      fill_in "user[last_name]", with: "McTesterson"
-      fill_in "user[email]", with: "test@test.com"
-      fill_in "user[password]", with: @admin.password
-      fill_in "user[password_confirmation]", with: "wrong"
+      fill_in 'user[first_name]', with: 'Testy'
+      fill_in 'user[last_name]', with: 'McTesterson'
+      fill_in 'user[email]', with: 'test@test.com'
+      fill_in 'user[password]', with: @admin.password
+      fill_in 'user[password_confirmation]', with: 'wrong'
 
-      click_on "Update"
+      click_on 'Update'
 
       expect(current_path).to eq(edit_user_path(@admin))
-      expect(page).to have_content("Password confirmation doesn't match Password")
+      expect(page).to have_content("Password confirmation doesn't match
+                                    Password")
     end
 
   end
