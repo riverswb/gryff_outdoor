1dba6cc0fd3b1fa96f12019a807ec238b83efa3e
Brett Rivers <riverswb@gmail.com>
2017-01-10 16:43:04 -0700

    Form in edit is complete

--- spec/features/admins/admin_can_edit_items_spec.rb
+++ spec/features/admins/admin_can_edit_items_spec.rb
@@ -19,11 +19,18 @@ RSpec.feature 'Admin can edit items' do
 
     expect(current_path).to eq(edit_admin_item_path(@item))
 
-    fill_in 'item[title]', with: "TeSt"
+    fill_in 'item[title]', with: 'TeSt'
+    fill_in 'item[description]', with: 'Test description'
+    fill_in 'item[price]', with: @item.price + 1
 
-    click_on "Update"
+    click_on 'Update'
 
     expect(current_path).to eq(item_path(@item))
-    expect(page).to have_content("TeSt")
+    expect(page).to have_content('TeSt')
+    expect(page).to_not have_content(@item.title)
+    expect(page).to have_content('Test description')
+    expect(page).to_not have_content(@item.description)
+    expect(page).to have_content("$#{@item.price + 1}")
   end
+
 end
