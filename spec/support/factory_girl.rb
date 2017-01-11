b33a4c276bc71cc4d1dcd80480b6438722bc1b3b
Brett Rivers <riverswb@gmail.com>
2017-01-11 16:54:05 -0700

    Fixes database cleaner

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -8,11 +8,26 @@ RSpec.configure do |config|
       DatabaseCleaner.clean
     end
   end
-  
+
   Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
+    end
+  end
+
+end
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
   end
 
