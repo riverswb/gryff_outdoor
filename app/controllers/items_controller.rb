049f47671ba3a6c417307df6a66a029a4717cbb8
Brett Rivers <riverswb@gmail.com>
2017-01-11 11:35:01 -0700

    Search bar can now search by item, category, or item and category

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   def index
     @search = Item.search(params[:q])
-    @items = @search.result
+    # @items = @search.result
     @q = Item.ransack(params[:q])
     @items = @q.result.includes(:category)
   end
